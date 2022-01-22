70ec083a03fb21f57776476de25fab4cbf29d71c
Pierre Keda <pierrekeda83@gmail.com>
2022-01-22 11:27:47 +0100

    Handle initial hash

--- src/main.js
+++ src/main.js
@@ -1,35 +1,38 @@
+import { GPGPU } from 'keda/three/gpgpu/GPGPU';
 import { Sketchpad } from 'keda/three/Sketchpad';
+
 import { Rain } from './sketches/rain/Rain';
 import { Navscan } from './sketches/navscan/Navscan';
 import { Blockflow } from './sketches/blockflow/Blockflow';
 import { Ablaze } from './sketches/ablaze/Ablaze';
 import { Backgrid } from './sketches/backgrid/Backgrid';
+import { Cyberdark } from './sketches/cyberdark/Cyberdark';
 
 const sketchpad = new Sketchpad( {
 	container: 'background',
 	//debug: true,
-	//fps: 240,
-	//fps: 2,
-	fps: 0,
-	stats: true,
+	//stats: true,
+	//fps: 60,
 } );
 
-const options = {
-	sketchpad,
-	gui: true,
-};
+GPGPU.init( sketchpad.renderer );
+
+const options = { sketchpad };
+
 const sketches = {
 	rain: () => new Rain( options ),
 	navscan: () => new Navscan( options ),
 	blockflow: () => new Blockflow( options ),
 	ablaze: () => new Ablaze( options ),
 	backgrid: () => new Backgrid( options ),
+	cyberdark: () => new Cyberdark( options ),
 };
 
-//const sketch = sketches.rain();
-const sketch = sketches.navscan();
-//const sketch = sketches.blockflow();
-//const sketch = sketches.ablaze();
-//const sketch = sketches.backgrid();
+const hash = window.location.hash.replace( '#', '' );
+const generator = sketches[ hash ] || sketches.navscan;
+
+const sketch = generator();
 
 sketchpad.init( sketch );
+
+document.title = `${sketch.settings.name} - Pierre Keda`;
